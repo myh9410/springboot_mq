8db12ff5235487c4702cc703e7364305bc1373c3
Moon <mjskks94@naver.com>
2022-12-25 22:19:29 +0900

    eventListner async 쓰레드 테스트

--- src/main/java/com/springboot/mq/config/EventComponent.java
+++ src/main/java/com/springboot/mq/config/EventComponent.java
@@ -1,2 +1,15 @@
-package com.springboot.mq.config;public class EventComponent {
+package com.springboot.mq.config;
+
+import com.springboot.mq.dto.event.TestEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventComponent {
+    @EventListener
+    @Async
+    public void eventListen(TestEvent testEvent) {
+        System.out.println("async 동작 :: " + Thread.currentThread().getId() + " :: " + testEvent.toString());
+    }
 }
