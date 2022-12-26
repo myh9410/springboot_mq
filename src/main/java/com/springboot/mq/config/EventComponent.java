b8913f275033e16853ee466f70d5984915053f1a
Moon <mjskks94@naver.com>
2022-12-26 17:47:12 +0900

    transactionaleventlistener 정리

--- src/main/java/com/springboot/mq/config/EventComponent.java
+++ src/main/java/com/springboot/mq/config/EventComponent.java
@@ -15,7 +15,23 @@ public class EventComponent {
 
     private final TestService testService;
 
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    /**
+     * EventListener의 경우, 상위 트랜잭션과 상관없이 동작
+     *
+     * TransactionalEventListener의 경우, 상위 트랜잭션에 영향을 받는다.
+     * TransactionPhase.AFTER_COMPLETION :
+     *      async 적용되어 다른 쓰레드 id로 실행 / DB insert 성공(트랜잭션 성공, 실패 여부와 상관없이 동작한다.)
+     * TransactionPhase.AFTER_COMMIT :
+     *      이벤트 동작 X (트랜잭션이 성공하지 않았기 때문에 이벤트가 동작하지 않는다.)
+     * TransactionPhase.BEFORE_COMMIT : 
+     *      트랜잭션의 커밋 전에 이벤트 실행이 되어야하는데
+     *      RuntimeException 발생 시 ROLLBACK 동작하여 이벤트 동작하지 않는것으로 보임
+     *      정상 동작한다면, async 적용되어 다른 쓰레드 id로 실행됨
+     *  TransactionPhase.AFTER_ROLLBACK :
+     *      정상 동작한다면, 이벤트 실행되지 않음.
+     *      ROLLBACK 발생 시 이벤트 동작하여 데이터 추가됨. (다른 쓰레드 id)
+     */
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
 //    @EventListener
     @Async
     public void eventListen(TestEvent testEvent) {
