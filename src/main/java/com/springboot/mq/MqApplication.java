865a0420f6d3d721048149ec3ecf7e487efaf86e
myh9410 <mjskks94@naver.com>
2022-12-30 15:36:54 +0900

    mysql db, jta관련 설정 추가

--- src/main/java/com/springboot/mq/MqApplication.java
+++ src/main/java/com/springboot/mq/MqApplication.java
@@ -1,12 +1,10 @@
 package com.springboot.mq;
 
 import com.springboot.mq.common.config.KafkaProperties;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties(value = {KafkaProperties.class})
 public class MqApplication {
